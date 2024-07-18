b08ad82632fe56358a6459f5e1ff09405abcf58d
madgallego <mharc.gallego@gmail.com>
2024-07-18 16:11:29 +0800

    WIP: Saving Progress

--- CC_JAVA/index.js
+++ CC_JAVA/index.js
@@ -1,4 +1,21 @@
-console.log("HELLO")
+/*console.log("HELLO")
 console.log("WORLD")
 
 window.alert("lmao")
+
+*/
+
+var arrNum =[];
+const size = 10;
+const max = 100;
+
+for(var i = 0; i<size; i++){
+    var randomNum = Math.floor(Math.random()*max)+1;
+                     //plus one to avoid value 0 because our minimum is 1
+                     console.log(randomNum);
+     arrNum.push = randomNum;
+}
+
+console.log(arrNum);
+
+
